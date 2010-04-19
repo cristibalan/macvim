d2ab492f7637451e5fa95a7e0b7f389ee8cf51b1
Cristi Balan <evil@che.lu>
2010-04-19 02:04:00 -0300

    make clean before building

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # from http://code.google.com/p/macvim/wiki/Building
 
 cd src
+make clean
 ./configure --enable-gui=macvim --with-features=huge --enable-cscope --enable-perlinterp --enable-pythoninterp --enable-rubyinterp --enable-sniff --enable-multibyte --with-macarchs=x86_64 --with-macsdk=10.6
 make
 cd MacVim
